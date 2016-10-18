e6c955fc2faf905682dcc187a4ee398afe2d6bb4
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-10-18 12:46:55 +0300

    converted all addict dicts to standard dictionaries

--- pyQueryConstructor.py
+++ pyQueryConstructor.py
@@ -74,7 +74,9 @@ class QueryConstructor():
         cquery.aggs["3"].aggs["51"].avg.field = "AvailableVCores"
         cquery.aggs["3"].aggs["52"].avg.field = "ContainerLaunchDurationNumOps"
         cquery.aggs["3"].aggs["53"].avg.field = "ContainerLaunchDurationAvgTime"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
     def systemLoadQuery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
                             qmin_doc_count=1):
@@ -97,7 +99,9 @@ class QueryConstructor():
         cquery.aggs["2"].aggs["1"].avg.field = "shortterm"
         cquery.aggs["2"].aggs["3"].avg.field = "midterm"
         cquery.aggs["2"].aggs["4"].avg.field = "longterm"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
     def systemMemoryQuery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
                             qmin_doc_count=1):
@@ -120,7 +124,8 @@ class QueryConstructor():
         cquery.aggs["2"].aggs["3"].terms.size = 0
         cquery.aggs["2"].aggs["3"].terms.order["1"] = "desc"
         cquery.aggs["2"].aggs["3"].aggs["1"].avg.field = "value"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
 
     def systemInterfaceQuery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
                             qmin_doc_count=1):
@@ -141,7 +146,9 @@ class QueryConstructor():
         cquery.aggs["3"].date_histogram.extended_bounds.max = qlte
         cquery.aggs["3"].aggs["1"].avg.field = "tx"
         cquery.aggs["3"].aggs["2"].avg.field = "rx"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
 
     def dfsQuery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
@@ -201,7 +208,9 @@ class QueryConstructor():
         cquery.aggs["3"].aggs["38"].avg.field = "GetImageAvgTime"
         cquery.aggs["3"].aggs["39"].avg.field = "PutImageNumOps"
         cquery.aggs["3"].aggs["40"].avg.field = "PutImageAvgTime"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
     def dfsFSQuery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
                             qmin_doc_count=1):
@@ -254,7 +263,9 @@ class QueryConstructor():
         cquery.aggs["34"].aggs["31"].avg.field = "BlockCapacity"
         cquery.aggs["34"].aggs["32"].avg.field = "StaleDataNodes"
         cquery.aggs["34"].aggs["33"].avg.field = "TotalFiles"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
     def jvmNNquery(self, qstring, qgte, qlte, qsize, qinterval, wildCard=True, qtformat="epoch_millis",
                             qmin_doc_count=1):
@@ -299,7 +310,9 @@ class QueryConstructor():
         cquery.aggs["13"].aggs["24"].avg.field = "LogFatal"
         cquery.aggs["13"].aggs["25"].avg.field = "LogWarn"
         cquery.aggs["13"].aggs["26"].avg.field = "LogInfo"
-        return cquery
+        cqueryd = cquery.to_dict()
+        return cqueryd
+
 
     def yarnQuery(self):
         return "Yarn metrics query"
