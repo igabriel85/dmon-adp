a75e8552b2c8ad9b5530d27292d37f780e70a6f0
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-10-19 17:10:25 +0300

    set logging to level ERROR

--- adplogger.py
+++ adplogger.py
@@ -2,7 +2,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger("Rotating Log")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 # add a rotating handler
 handler = TimedRotatingFileHandler('dmonadp.log', when="d", interval=1, backupCount=5)
