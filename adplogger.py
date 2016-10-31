3250a39d759eefda70eaacfd81c383ab49f6c542
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-10-31 14:36:56 +0200

    added default location for log file

--- adplogger.py
+++ adplogger.py
@@ -1,10 +1,13 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+import os
+import datetime
 
 logger = logging.getLogger("Rotating Log")
 logger.setLevel(logging.ERROR)
 
 # add a rotating handler
-handler = TimedRotatingFileHandler('dmonadp.log', when="d", interval=1, backupCount=5)
+logFile = os.path.join(os.path.dirname(os.path.abspath('')), 'dmonadp.log')
+handler = TimedRotatingFileHandler(logFile, when="d", interval=1, backupCount=5)
 logger.addHandler(handler)
 
